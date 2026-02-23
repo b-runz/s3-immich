8baeb82abc34c4614930bc90148e287347b13b63
Brandon Wees <brandonwees@gmail.com>
2026-02-23 14:57:57 -0600

    feat: getAssetEdits respond with edit IDs (#26445)
    * feat: getAssetEdits respond with edit IDs
    
    * chore: cleanup typings for edit API
    
    * chore: cleanup types with jason
    
    * fix: openapi sync
    
    * fix: factory

--- openapi/lib/model/asset_edit_action_item_dto.dart
+++ openapi/lib/model/asset_edit_action_item_dto.dart
@@ -10,9 +10,9 @@
 
 part of openapi.api;
 
-class AssetEditActionListDtoEditsInner {
-  /// Returns a new [AssetEditActionListDtoEditsInner] instance.
-  AssetEditActionListDtoEditsInner({
+class AssetEditActionItemDto {
+  /// Returns a new [AssetEditActionItemDto] instance.
+  AssetEditActionItemDto({
     required this.action,
     required this.parameters,
   });
@@ -20,10 +20,10 @@ class AssetEditActionListDtoEditsInner {
   /// Type of edit action to perform
   AssetEditAction action;
 
-  MirrorParameters parameters;
+  AssetEditActionItemDtoParameters parameters;
 
   @override
-  bool operator ==(Object other) => identical(this, other) || other is AssetEditActionListDtoEditsInner &&
+  bool operator ==(Object other) => identical(this, other) || other is AssetEditActionItemDto &&
     other.action == action &&
     other.parameters == parameters;
 
@@ -34,7 +34,7 @@ class AssetEditActionListDtoEditsInner {
     (parameters.hashCode);
 
   @override
-  String toString() => 'AssetEditActionListDtoEditsInner[action=$action, parameters=$parameters]';
+  String toString() => 'AssetEditActionItemDto[action=$action, parameters=$parameters]';
 
   Map<String, dynamic> toJson() {
     final json = <String, dynamic>{};
@@ -43,27 +43,27 @@ class AssetEditActionListDtoEditsInner {
     return json;
   }
 
-  /// Returns a new [AssetEditActionListDtoEditsInner] instance and imports its values from
+  /// Returns a new [AssetEditActionItemDto] instance and imports its values from
   /// [value] if it's a [Map], null otherwise.
   // ignore: prefer_constructors_over_static_methods
-  static AssetEditActionListDtoEditsInner? fromJson(dynamic value) {
-    upgradeDto(value, "AssetEditActionListDtoEditsInner");
+  static AssetEditActionItemDto? fromJson(dynamic value) {
+    upgradeDto(value, "AssetEditActionItemDto");
     if (value is Map) {
       final json = value.cast<String, dynamic>();
 
-      return AssetEditActionListDtoEditsInner(
+      return AssetEditActionItemDto(
         action: AssetEditAction.fromJson(json[r'action'])!,
-        parameters: MirrorParameters.fromJson(json[r'parameters'])!,
+        parameters: AssetEditActionItemDtoParameters.fromJson(json[r'parameters'])!,
       );
     }
     return null;
   }
 
-  static List<AssetEditActionListDtoEditsInner> listFromJson(dynamic json, {bool growable = false,}) {
-    final result = <AssetEditActionListDtoEditsInner>[];
+  static List<AssetEditActionItemDto> listFromJson(dynamic json, {bool growable = false,}) {
+    final result = <AssetEditActionItemDto>[];
     if (json is List && json.isNotEmpty) {
       for (final row in json) {
-        final value = AssetEditActionListDtoEditsInner.fromJson(row);
+        final value = AssetEditActionItemDto.fromJson(row);
         if (value != null) {
           result.add(value);
         }
@@ -72,12 +72,12 @@ class AssetEditActionListDtoEditsInner {
     return result.toList(growable: growable);
   }
 
-  static Map<String, AssetEditActionListDtoEditsInner> mapFromJson(dynamic json) {
-    final map = <String, AssetEditActionListDtoEditsInner>{};
+  static Map<String, AssetEditActionItemDto> mapFromJson(dynamic json) {
+    final map = <String, AssetEditActionItemDto>{};
     if (json is Map && json.isNotEmpty) {
       json = json.cast<String, dynamic>(); // ignore: parameter_assignments
       for (final entry in json.entries) {
-        final value = AssetEditActionListDtoEditsInner.fromJson(entry.value);
+        final value = AssetEditActionItemDto.fromJson(entry.value);
         if (value != null) {
           map[entry.key] = value;
         }
@@ -86,14 +86,14 @@ class AssetEditActionListDtoEditsInner {
     return map;
   }
 
-  // maps a json object with a list of AssetEditActionListDtoEditsInner-objects as value to a dart map
-  static Map<String, List<AssetEditActionListDtoEditsInner>> mapListFromJson(dynamic json, {bool growable = false,}) {
-    final map = <String, List<AssetEditActionListDtoEditsInner>>{};
+  // maps a json object with a list of AssetEditActionItemDto-objects as value to a dart map
+  static Map<String, List<AssetEditActionItemDto>> mapListFromJson(dynamic json, {bool growable = false,}) {
+    final map = <String, List<AssetEditActionItemDto>>{};
     if (json is Map && json.isNotEmpty) {
       // ignore: parameter_assignments
       json = json.cast<String, dynamic>();
       for (final entry in json.entries) {
-        map[entry.key] = AssetEditActionListDtoEditsInner.listFromJson(entry.value, growable: growable,);
+        map[entry.key] = AssetEditActionItemDto.listFromJson(entry.value, growable: growable,);
       }
     }
     return map;
